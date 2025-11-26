449ee99794c3c9e8b1c2a26a0eca53f1a3943b51
ginger-beaver <nicky.um@hotmail.com>
2025-11-26 15:06:02 +0300

    Add users api client

---
+++ clients/http/gateway/users/client.py
@@ -0,0 +1,22 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.http.client import HTTPClient
+
+
+class CreateUserRequestDict(TypedDict):
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+    phoneNumber: str
+
+
+class UsersGatewayHTTPClient(HTTPClient):
+
+    def get_user_api(self, user_id: str) -> Response:
+        return self.get(f"/api/v1/users/{user_id}")
+
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        return self.post("/api/v1/users", json=request)
