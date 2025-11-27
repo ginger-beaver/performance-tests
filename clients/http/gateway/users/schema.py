81cce7796380f768d1d3e6a1b17b3a9218dd7ac1
ginger-beaver <nicky.um@hotmail.com>
2025-11-27 15:25:56 +0300

    Add faker support for creating user

--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,7 +1,7 @@
-from pydantic import EmailStr
+from pydantic import EmailStr, Field
 
 from tools.camel_model import CamelModel
-
+from tools.fakers import fake
 
 class UserSchema(CamelModel):
     id: str
@@ -17,11 +17,11 @@ class GetUserResponseSchema(CamelModel):
 
 
 class CreateUserRequestSchema(CamelModel):
-    email: EmailStr
-    last_name: str
-    first_name: str
-    middle_name: str
-    phone_number: str
+    email: EmailStr = Field(default_factory=fake.email)
+    last_name: str = Field(default_factory=fake.last_name)
+    first_name: str = Field(default_factory=fake.first_name)
+    middle_name: str = Field(default_factory=fake.middle_name)
+    phone_number: str = Field(default_factory=fake.phone_number)
 
 
 class CreateUserResponseSchema(CamelModel):
