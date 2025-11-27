04229a4e59d721865fd0ac35a1c7ebc0ab956794
ginger-beaver <nicky.um@hotmail.com>
2025-11-27 14:19:48 +0300

    Add pydantic models support to user api client

--- clients/http/gateway/users/client.py
+++ clients/http/gateway/users/client.py
@@ -1,16 +1,13 @@
-from typing import TypedDict
+import time
 
 from httpx import Response
 
 from clients.http.base_client import HTTPClient
-
-
-class CreateUserRequestDict(TypedDict):
-    email: str
-    lastName: str
-    firstName: str
-    middleName: str
-    phoneNumber: str
+from clients.http.gateway.users.schema import (
+    GetUserResponseSchema,
+    CreateUserRequestSchema,
+    CreateUserResponseSchema
+)
 
 
 class UsersGatewayHTTPClient(HTTPClient):
@@ -18,5 +15,20 @@ class UsersGatewayHTTPClient(HTTPClient):
     def get_user_api(self, user_id: str) -> Response:
         return self.get(f"/api/v1/users/{user_id}")
 
-    def create_user_api(self, request: CreateUserRequestDict) -> Response:
-        return self.post("/api/v1/users", json=request)
+    def create_user_api(self, request: CreateUserRequestSchema) -> Response:
+        return self.post("/api/v1/users", json=request.model_dump(by_alias=True))
+
+    def get_user(self, user_id: str) -> GetUserResponseSchema:
+        response = self.get_user_api(user_id)
+        return GetUserResponseSchema.model_validate_json(response.text)
+
+    def create_user(self) -> CreateUserResponseSchema:
+        request = CreateUserRequestSchema(
+            email=f"user.{time.time()}@example.com",
+            last_name="string",
+            first_name="string",
+            middle_name="string",
+            phone_number="string"
+        )
+        response = self.create_user_api(request)
+        return CreateUserResponseSchema.model_validate_json(response.text)
