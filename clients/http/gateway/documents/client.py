b4810868e09cec4a5a67ee0f8590ece459be32d6
ginger-beaver <nicky.um@hotmail.com>
2025-11-27 14:43:26 +0300

    Add pydantic models support to documents api client

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,6 +1,10 @@
 from httpx import Response
 
 from clients.http.base_client import HTTPClient
+from clients.http.gateway.documents.schema import (
+    GetTariffDocumentResponseSchema,
+    GetContractDocumentResponseSchema
+)
 
 
 class DocumentsGatewayHTTPClient(HTTPClient):
@@ -10,3 +14,11 @@ class DocumentsGatewayHTTPClient(HTTPClient):
 
     def get_contract_document_api(self, account_id: str) -> Response:
         return self.get(f"/api/v1/documents/contract-document/{account_id}")
+
+    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseSchema:
+        response = self.get_tariff_document_api(account_id)
+        return GetTariffDocumentResponseSchema.model_validate_json(response.text)
+
+    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseSchema:
+        response = self.get_contract_document_api(account_id)
+        return GetContractDocumentResponseSchema.model_validate_json(response.text)
