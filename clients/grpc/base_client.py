32350fd938c9643d00cd6ee4e68c35fc65af7dfc
ginger-beaver <nicky.um@hotmail.com>
2025-11-28 17:05:24 +0300

    Add base grpc client

---
+++ clients/grpc/base_client.py
@@ -0,0 +1,10 @@
+import grpc.experimental.gevent as grpc_gevent
+
+from grpc import Channel
+
+grpc_gevent.init_gevent()
+
+
+class GRPCClient:
+    def __init__(self, channel: Channel):
+        self.channel = channel
