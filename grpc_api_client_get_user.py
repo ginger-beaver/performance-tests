f587c6455331b58b8aad1286e8157e34d5d0c55d
ginger-beaver <nicky.um@hotmail.com>
2025-11-28 17:43:44 +0300

    Add script using gRPC client to create and get a user

---
+++ grpc_api_client_get_user.py
@@ -0,0 +1,10 @@
+from clients.grpc.gateway.client_builder import GRPCClientBuilder
+
+builder = GRPCClientBuilder()
+users_gateway_client = builder.build_users_gateway()
+
+create_user_response = users_gateway_client.create_user()
+print('Create user data:', create_user_response)
+
+get_user_response = users_gateway_client.get_user(create_user_response.user.id)
+print('Get user data:', get_user_response)
