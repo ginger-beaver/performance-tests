f56671fe117f8a343a7cf12068ee1e3852977500
ginger-beaver <nicky.um@hotmail.com>
2025-11-29 18:08:41 +0300

    Add script using gRPC client to fetch documents

---
+++ grpc_api_client_get_documents.py
@@ -0,0 +1,25 @@
+from clients.grpc.gateway.client_builder import GRPCClientBuilder
+
+grpc_builder = GRPCClientBuilder()
+
+users_gateway_client = grpc_builder.build_users_gateway()
+accounts_gateway_client = grpc_builder.build_accounts_gateway()
+documents_gateway_client = grpc_builder.build_documents_gateway()
+
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+open_debit_card_account_response = accounts_gateway_client.open_credit_card_account(
+    user_id=create_user_response.user.id
+)
+print('Open credit card account response:', open_debit_card_account_response)
+
+get_tariff_document_response = documents_gateway_client.get_tariff_document(
+    account_id=open_debit_card_account_response.account.id
+)
+print('Get tariff document response:', get_tariff_document_response)
+
+get_contract_document_response = documents_gateway_client.get_contract_document(
+    account_id=open_debit_card_account_response.account.id
+)
+print('Get contract document response:', get_contract_document_response)
