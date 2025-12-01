5511901d5de00630d4f7a3565873bfc1b66556c1
ginger-beaver <nicky.um@hotmail.com>
2025-12-01 16:33:22 +0300

    Add factory function creating clients for performance tests

--- clients/http/gateway/client_factory.py
+++ clients/http/gateway/client_factory.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Callable, Type
 
 from httpx import Client
+from locust.env import Environment
 
 from clients.http.base_client import HTTPClient
+from clients.http.event_hooks.locust_event_hook import locust_request_event_hook, locust_response_event_hook
 
 
 class HTTPClientFactory:
@@ -15,3 +18,16 @@ class HTTPClientFactory:
 
 def build_gateway_http_client() -> Client:
     return Client(timeout=100, base_url="http://localhost:8003")
+
+
+def build_gateway_locust_http_client(environment: Environment) -> Client:
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
+    return Client(
+        timeout=100,
+        base_url="http://localhost:8003",
+        event_hooks={
+            "request": [locust_request_event_hook],
+            "response": [locust_response_event_hook(environment)]
+        }
+    )
