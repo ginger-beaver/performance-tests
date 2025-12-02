1f6aca5aae0019eee8f953015184b1e15647d1a9
ginger-beaver <nicky.um@hotmail.com>
2025-12-02 13:39:34 +0300

    Add factory function for building grpc clients with locust metrics

--- clients/grpc/gateway/client_factory.py
+++ clients/grpc/gateway/client_factory.py
@@ -1,8 +1,10 @@
 from typing import Callable, Type
 
-from grpc import Channel, insecure_channel
+from grpc import Channel, insecure_channel, intercept_channel
+from locust.env import Environment
 
 from clients.grpc.base_client import GRPCClient
+from clients.grpc.interceptors.locust_interceptor import LocustInterceptor
 
 
 class GRPCClientFactory:
@@ -15,3 +17,11 @@ class GRPCClientFactory:
 
 def build_gateway_grpc_client() -> Channel:
     return insecure_channel("localhost:9003")
+
+
+def build_gateway_locust_grpc_client(environment: Environment) -> Channel:
+    locust_interceptor = LocustInterceptor(environment=environment)
+
+    channel = insecure_channel("localhost:9003")
+
+    return intercept_channel(channel, locust_interceptor)
