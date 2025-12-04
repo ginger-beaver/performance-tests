df998a1e5d2f7bf0fdb50f94f3bcd54866a8ba65
ginger-beaver <nicky.um@hotmail.com>
2025-12-04 17:15:29 +0300

    Add get operations locust scenario for existing user

---
+++ scenarios/http/gateway/existing_user_get_operations/scenario.py
@@ -0,0 +1,45 @@
+import logging
+
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from seeds.builder import build_grpc_seeds_builder
+from seeds.scenarios.existing_user_get_operations import ExistingUserGetOperationsSeedsScenario
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+logger = logging.getLogger("locust")
+
+
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    logger.info("Add seed data to locust environment")
+    seeds_scenario = ExistingUserGetOperationsSeedsScenario(build_grpc_seeds_builder)
+    seeds_scenario.build()
+
+    environment.seeds = seeds_scenario.load()
+
+
+class GetOperationsTaskSet(GatewayHTTPTaskSet):
+    seed_user: SeedUserResult
+
+    def on_start(self) -> None:
+        super().on_start()
+        self.seed_user = self.user.environment.seeds.get_random_user()
+
+    @task(1)
+    def get_accounts(self):
+        self.accounts_gateway_client.get_accounts(self.seed_user.user_id)
+
+    @task(2)
+    def get_operations(self):
+        self.operations_gateway_client.get_operations(self.seed_user.credit_card_accounts[0].account_id)
+
+    @task(2)
+    def get_operations_summary(self):
+        self.operations_gateway_client.get_operations_summary(self.seed_user.credit_card_accounts[0].account_id)
+
+
+class GetOperationsScenarioUser(LocustBaseUser):
+    tasks = [GetOperationsTaskSet]
